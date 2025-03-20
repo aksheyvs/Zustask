a26afa55c612136f10077b82aab9bc7045187bab
akshey <aksheyvs@gmail.com>
2025-03-20 11:43:18 +0530

    Initialize Tailwind CSS setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
-import "./App.css";
-
 function App() {
-    return <></>;
+    return (
+        <>
+            <h1 className="text-red-500">Hello</h1>
+        </>
+    );
 }
 
 export default App;
